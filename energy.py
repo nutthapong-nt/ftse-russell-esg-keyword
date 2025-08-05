a3ff339fa0864e128b909ebd4583e5f0bef26131
nutthapong.pi <nutthapong.pi@inet.co.th>
2025-08-05 18:47:14 +0700

    Add FTSE keyword

---
+++ energy.py
@@ -0,0 +1,32 @@
+BIODIVERSITY = []
+CLIMATE_CHANGE = [
+    "ประสิทธิภาพมากขึ้น|efficiency"
+]
+POLLUTION_RESOURCES = []
+WATER_SECURITY = []
+CUSTOMER_RESPONSIBILITY = [
+    "เน็ตซีโร่|เน็ต Zero|net zero",
+    "คาร์บอนฟุตปริ้น",
+    "การปล่อยคาร์บอน",
+    "การปลดปล่อยก๊าซธรรมชาติ",
+    "ปล่อยก๊าซเรือนกระจก",
+    "สารปนเปื้อน",
+    "การปล่อยน้ำเสีย",
+    "ลดการปลดปล่อย",
+    "ลดการเผา",
+    "ลดคาร์บอน",
+    "PM 2.5",
+    "Zero Waste",
+    "รีไซเคิลน้ำ|รีไซเคิล",
+]
+HEALTH_SAFETY = [
+    "ระบบการจัดการฝุ่น",
+]
+HUMAN_RIGHTS_COMMUNITY = []
+LABOR_STANDARD = []
+ANTI_CORRUPTION = []
+CORPORATE_GOVERNANCE = []
+RISK_MANAGEMENT = []
+TAX_TRANSPARENCY = []
+SUPPLY_CHAIN_ENVIRONMENTAL = []
+SUPPLY_CHAIN_SOCIAL = []
