601a79a45d219dc72b9547507b939abe14df3474
nutthapong.pi <nutthapong.pi@inet.co.th>
2025-08-18 10:05:29 +0700

    Add Keyword BTG

--- food.py
+++ food.py
@@ -1,4 +1,11 @@
-from common import NATURAL
+from common import BIO_SECURITY, FOOD_SAFETY, NATURAL
 
 
-CUSTOMER_RESPONSIBILITY = [NATURAL,"ล้นตลาด","ขาดตลาด","การตัดราคา"]
+CUSTOMER_RESPONSIBILITY = [
+    NATURAL,
+    "ล้นตลาด",
+    "ขาดตลาด",
+    "การตัดราคา",
+    FOOD_SAFETY,
+    BIO_SECURITY,
+]
