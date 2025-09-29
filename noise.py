932597745b6b5b454a3dd9b4da450e3335c49479
nutthapong.pi <nutthapong.pi@inet.co.th>
2025-09-30 00:01:27 +0700

    Rename junkword to noise

--- noise.py
+++ noise.py
@@ -1,4 +1,8 @@
-JUNKWORD = [
+# I try from https://github.com/PyThaiNLP/pythainlp/blob/dev/pythainlp/corpus/stopwords_th.txt 
+# But it is not suitable for this case; it crashes many keywords.
+# So I try to create a stop word list from Oppday manually.
+
+STOPWORD = [
     "สำหรับ",
     "นะครับ",
     "นะคะ",
