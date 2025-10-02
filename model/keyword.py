ec573c33da48d067bbc86d30928c49986c3363ab
nutthapong.pi <nutthapong.pi@inet.co.th>
2025-10-02 19:37:41 +0700

    keyword validation

--- model/keyword.py
+++ model/keyword.py
@@ -3,22 +3,42 @@ from typing import List
 
 from model.local_keyword import LOCAL_KEYWORD
 
+
 @dataclass
 class Keyword:
-    
+
     mainword: str
     word: str
     topic: List[str] = field(default_factory=list)
 
     def __init__(self, mainword: str, word: str, topic: List[str]):
-        self.mainword = mainword   # the variable name e.g. CARBON_NEUTRALITY
-        self.word = word           # one synonym word
-        self.topic = topic         # list of topics
+        self.mainword = mainword  # the variable name e.g. CARBON_NEUTRALITY
+        self.word = word  # one synonym word
+        self.topic = topic  # list of topics
 
     def __repr__(self):
         return f"Keyword(mainword='{self.mainword}', word='{self.word}', topic={self.topic})"
-    
+
+def validate_keyword_list(data):
+    if not isinstance(data, list):
+        raise TypeError("keyword list be array")
+    for entry in data:
+        validate_keyword_object(entry)
+
+def validate_keyword_object(entry):
+    if not isinstance(entry, dict):
+        raise TypeError("data in keyword list must be object")
+    if "keyword" not in entry or "topic" not in entry or "synonym" not in entry:
+        raise TypeError("keyword object must contain keyword, topic, and synonym keys")
+    if not isinstance(entry["synonym"], list):
+        raise TypeError("synonym must be array")
+    for synonym in entry["synonym"]:
+        if not isinstance(synonym, str):
+            raise TypeError("synonym word must be string")
+
+
 def get_keyword_list(data=LOCAL_KEYWORD):
+    validate_keyword_list(data)
     keywords: List[Keyword] = []
     for entry in data:
         mainword = entry["keyword"]
